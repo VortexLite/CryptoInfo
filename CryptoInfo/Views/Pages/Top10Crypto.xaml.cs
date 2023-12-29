fad837b9880cd0966b58efca9800840618784e0c
VortexLite <minedinnerbone@gmail.com>
2023-12-29 15:13:27 +0200

    Створив першу сторінку топ 10 крипти

---
+++ CryptoInfo/Views/Pages/Top10Crypto.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CryptoInfo.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Top10Crypto.xaml
+    /// </summary>
+    public partial class Top10Crypto : Page
+    {
+        public Top10Crypto()
+        {
+            InitializeComponent();
+        }
+    }
+}
